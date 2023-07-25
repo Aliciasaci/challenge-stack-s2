807c31f9d1f8ab458922f1109f4d634958d629f7
Awa Bah <bah.awaa@gmail.com>
2023-07-25 17:59:24 +0200

    tagrouter test and user routertest

--- serveur/src/db/models/user.js
+++ serveur/src/db/models/user.js
@@ -1,65 +1,68 @@
 module.exports = function (connection) {
   const { DataTypes, Model } = require("sequelize");
   const bcrypt = require("bcryptjs");
-  const User = connection.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    firstname: {
-      type: DataTypes.STRING,
-      validate: {
-        len: [8, 32],
-        //is: /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/i,
+  const User = connection.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        validate: {
+          //len: [8, 32],
+          //is: /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/i,
+        },
+      },
+      lastname: {
+        type: DataTypes.STRING,
+      },
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "USER_CLIENT",
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true,
+        },
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // validate: {
+        //   len: [8, 32],
+        //   is: /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/i,
+        // },
+      },
+      societe: {
+        type: DataTypes.STRING,
+      },
+      kbis: {
+        type: DataTypes.STRING,
+      },
+      telephone: {
+        type: DataTypes.INTEGER,
+      },
+      urlsite: {
+        type: DataTypes.STRING,
+      },
+      compteIsVerified: {
+        type: DataTypes.STRING,
+      },
+      appId: {
+        type: DataTypes.STRING,
       },
     },
-    lastname: {
-      type: DataTypes.STRING,
-    },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: "USER_CLIENT",
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true,
-      },
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      // validate: {
-      //   len: [8, 32],
-      //   is: /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/i,
-      // },
-    },
-    societe: {
-      type: DataTypes.STRING
-    },
-    kbis: {
-      type: DataTypes.STRING
-    },
-    telephone: {
-      type: DataTypes.INTEGER
-    },
-    urlsite: {
-      type: DataTypes.STRING
-    },
-    compteIsVerified: {
-      type: DataTypes.STRING
-    },
-    appId: {
-      type: DataTypes.STRING
+    {
+      connection,
+      tableName: "users",
     }
-    
-  }, {
-    connection,
-    tableName: "users",
-  });
+  );
 
   async function hashPassword(user) {
     console.log("hashPassword");
--- serveur/src/server.js
+++ serveur/src/server.js
@@ -35,6 +35,10 @@ app.use(
 
 app.use(errorsHandler);
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(3000, () => {
+    console.log("App listening on port 3000!");
+  });
+}
+
+module.exports = app;
