712835b99c479d424ba794898b09e695a2ee5f52
BahAwa <bah.awaa@gmail.com>
2023-07-30 14:43:05 +0200

    hard coded db url

--- serveur/src/db/db.js
+++ serveur/src/db/db.js
@@ -3,9 +3,12 @@ const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 
 console.log(process.env.DATABASE_URL);
-const connection = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-});
+const connection = new Sequelize(
+  "postgres://challenge:root@79.137.86.197:5432/postgres_test",
+  {
+    dialect: "postgres",
+  }
+);
 
 connection
   .authenticate()
@@ -15,21 +18,20 @@ connection
   .catch((error) => {
     console.error("Connexion à POSTGRES échouée:", error);
   });
-  console.log(process.env.NODE_ENV);
-  //*MongoDB
-  const mongoose = require("mongoose");
-  console.log(process.env.MONGO_URL)
-  mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("=> Connexion à MongoDB réussie");
-    })
-    .catch((error) => {
-      console.error("Erreur de connexion à MongoDB :", error);
-    });
-
+console.log(process.env.NODE_ENV);
+//*MongoDB
+const mongoose = require("mongoose");
+console.log(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("=> Connexion à MongoDB réussie");
+  })
+  .catch((error) => {
+    console.error("Erreur de connexion à MongoDB :", error);
+  });
 
 module.exports = connection;
